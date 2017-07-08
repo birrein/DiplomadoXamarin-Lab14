36e99f80ccd96fdf6f865128827cf66261aaec6a
Manuel Marín <birrein@LIVE.COM>
2017-07-07 21:45:31 -0400

    Add project files.

---
+++ Lab14/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab14
+{
+    [Activity(Label = "Lab14", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var ButtonValidate = FindViewById<Button>(Resource.Id.buttonValidate);
+
+            ButtonValidate.Click += async (sender, e) =>
+            {
+                var Client = new SALLab14.ServiceClient();
+                var Result = await Client.ValidateAsync(this);
+
+                Android.App.AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+                AlertDialog Alert = Builder.Create();
+                Alert.SetTitle("Resultado de la verificación");
+                Alert.SetIcon(Resource.Drawable.Icon);
+                Alert.SetMessage($"{Result.Status}\n{Result.FullName}\n{Result.Token}");
+                Alert.SetButton("Ok", (s, ev) => { });
+                Alert.Show();
+            };
+        }
+    }
+}
+
